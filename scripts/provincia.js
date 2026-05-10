bf29944e893421aad48e1dcdc12969c377e534a1
girishsardar <girishsardar44@gmail.com>
2026-05-10 21:58:21 +0200

    chore(injection): first successful injection scripts

---
+++ scripts/provincia.js
@@ -0,0 +1,8 @@
+const provincia = () => {
+    const url = "https://icp.administracionelectronica.gob.es/icpplus/index.html"
+    if (url === "https://icp.administracionelectronica.gob.es/icpplus/index.html"){
+        const dropdow = window.document.querySelector('#form').options[9].selected= true;
+   }
+    const accept = window.document.querySelector('#btnAceptar').click()
+};
+provincia();
