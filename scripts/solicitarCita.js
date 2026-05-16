9c8ead619eefb397be8bd6171f154961405ffa4d
girishsardar <girishsardar44@gmail.com>
2026-05-16 13:09:10 +0200

    chore(scripts): moved scripts folder to apps dir

--- scripts/solicitarCita.js
+++
@@ -1,7 +0,0 @@
-const solicitCita = () => {
- const url = "https://icp.administracionelectronica.gob.es/icpplustieb/acValidarEntrada"
- if(url === "https://icp.administracionelectronica.gob.es/icpplustieb/acValidarEntrada"){
-  window.document.querySelector("#btnEnviar").click()
- }
-}
-solicitCita();
