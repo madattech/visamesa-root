452bf18d88a32113a70fda903eb51983930e7a2f
gs-44 <girishsardar44@gmail.com>
2026-08-02 18:48:27 +0200

    fix(android): fixed api url for android as localhost rather fixed address

--- apps/mobile/src/config/api.ts
+++ apps/mobile/src/config/api.ts
@@ -7,7 +7,7 @@ import { Platform } from 'react-native';
 export const API_BASE_URL = __DEV__
   ? Platform.select({
       ios: 'http://localhost:3000',
-      android: 'http://10.0.2.2:3000',
+      android: 'http://localhost:3000',
       default: 'http://localhost:3000',
     })!
   : 'https://visamesa-be-1052558133908.europe-west1.run.app';
